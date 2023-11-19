c0f0534085c5cccc7394a8ebef2c4597805e6689
Jon Craig <jcraig11@uco.edu>
2023-11-19 13:35:03 -0600

    implemented tables for database

--- Database/init.sql
+++ Database/init.sql
@@ -1,11 +1,28 @@
--- Create a sample table
-CREATE TABLE IF NOT EXISTS mytable (
-    id serial PRIMARY KEY,
-    name VARCHAR (255) NOT NULL
+-- Create the database for users, chats, and settings
+CREATE DATABASE SEII_LLM_Frontend;
+
+-- Connect to the newly created database
+\c SEII_LLM_Frontend;
+
+-- Create a table for users
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(100) NOT NULL
 );
 
--- Insert some sample data
-INSERT INTO mytable (name) VALUES
-('John'),
-('Jane'),
-('Alice');
+-- Create a table for chats
+CREATE TABLE chats (
+    chat_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    chat_content TEXT[]
+);
+
+-- Create a table for user settings
+CREATE TABLE user_settings (
+    user_id INT REFERENCES users(user_id),
+    dark_mode BOOLEAN DEFAULT FALSE,
+    temperature INT CHECK (temperature >= 1 AND temperature <= 5),
+    typing_speed INT CHECK (typing_speed >= 1 AND typing_speed <= 5),
+    PRIMARY KEY (user_id)
+);
