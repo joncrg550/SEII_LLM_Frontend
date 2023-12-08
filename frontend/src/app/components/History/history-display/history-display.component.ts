77214157c61cf3ef438fc2873f70f4690d332385
Jon Craig <jcraig11@uco.edu>
2023-12-08 02:59:59 -0600

    started working history

--- frontend/src/app/components/History/history-display/history-display.component.ts
+++ frontend/src/app/components/History/history-display/history-display.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { ChatService } from 'src/app/services/chat-service/chat.service';
 @Component({
   selector: 'history-display',
   templateUrl: './history-display.component.html',
   styleUrls: ['./history-display.component.css']
 })
 export class HistoryDisplayComponent {
+  constructor(private chatService: ChatService) {}
 
+  displayChatMessages() {
+    return this.chatService.getChatMessages();
+  }
 }
